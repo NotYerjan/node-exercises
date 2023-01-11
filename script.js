569b5441bf859340835d763a99a04f2700769cbb
Yerjan <yerjan@proton.me>
2023-01-11 17:31:20 +0300

    ignore node modules

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-var figlet = require("figlet");
-
-figlet("Welcome to our web!", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
+//ex8.txt
